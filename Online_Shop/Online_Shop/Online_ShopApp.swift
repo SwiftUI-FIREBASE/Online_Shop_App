660b2c039e12456586b53847557a7410b8386d5c
LuisMandM <129441609+LuisMandM@users.noreply.github.com>
2024-02-23 18:55:03 +0100

    Add files via upload

---
+++ Online_Shop/Online_Shop/Online_ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Online_ShopApp.swift
+//  Online_Shop
+//
+//  Created by  on 23/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Online_ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
